afaadd258ca93fac45fcb86e04fa1dd1a4680c5d
ws8149 <wilson@innoractive.com>
2022-08-11 15:20:16 +0800

    add hardcoded popular food

--- lib/pages/FoodHomePage.dart
+++ lib/pages/FoodHomePage.dart
@@ -75,37 +75,47 @@ class LoadedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(25),
+    return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Delivering to', style: TextStyle(color: Colors.grey)),
+          SizedBox(height: 20),
 
-          Row(
-            children: [
-              Text(
-                'Current Location',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20
+          Padding(
+            padding: const EdgeInsets.only(left: 25,right: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Delivering to', style: TextStyle(color: Colors.grey)),
+
+                Row(
+                  children: [
+                    Text(
+                      'Current Location',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                      ),
+                    ),
+                    SizedBox(width: 30),
+                    Icon(Icons.arrow_drop_down, color: Colors.red.shade800)
+                  ],
                 ),
-              ),
-              SizedBox(width: 30),
-              Icon(Icons.arrow_drop_down, color: Colors.red.shade800)
-            ],
+              ],
+            ),
           ),
+          SizedBox(height: 20),
 
-          SizedBox(height: 30),
-
-          SearchBar(),
-
-          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left:25 , right: 25),
+            child: SearchBar(),
+          ),
 
           Container(
             height: 120,
             child: ListView.builder(
+              padding: EdgeInsets.only(left: 10, right: 10),
               itemCount: state.foodCategoryList.length,
               scrollDirection: Axis.horizontal,
               itemBuilder: (_, index) {
@@ -144,6 +154,20 @@ class LoadedPage extends StatelessWidget {
             ),
           ),
 
+          SizedBox(height: 20),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Popular Food', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+          ),
+
+          SizedBox(height: 20),
+
+          PopularCard(),
+          SizedBox(height: 20),
+          PopularCard(),
+          SizedBox(height: 20),
+          PopularCard(),
 
           ElevatedButton(
               onPressed: () {
@@ -153,6 +177,48 @@ class LoadedPage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class PopularCard extends StatelessWidget {
+  const PopularCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image(
+          image: AssetImage('lib/assets/hardcoded.jpg'),
+          width: MediaQuery.of(context).size.width,
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Hardcoded by Wilson', style: TextStyle(fontWeight: FontWeight.bold)),
+
+              Row(
+                children: [
+                  Icon(Icons.star, color: Colors.red.shade800),
+                  Text('4.9', style: TextStyle(color: Colors.red.shade800)),
+                  SizedBox(width: 5),
+                  Text('(124 Ratings) Cafe', style: TextStyle(color: Colors.grey)),
+                  SizedBox(width: 5),
+                  Icon(Icons.circle, color: Colors.red.shade800, size: 4),
+                  SizedBox(width: 5),
+                  Text('Western Food', style: TextStyle(color: Colors.grey)),
+                ],
+              )
+            ],
+          ),
+        )
+
+      ],
     );
   }
 }
